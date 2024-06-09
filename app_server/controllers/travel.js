d257b21ba0968a8df4b42d6581afd912c474c1c1
Binaya Rimal <rimal.binaya@gmail.com>
2024-06-09 16:07:38 -0700

    mod-5 completed

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,10 +1,26 @@
 /* GET Homepage */
 
-var fs = require('fs');
-var trips = JSON.parse(fs.readFileSync('data/trips.json', 'utf8'));
+// var fs = require('fs');
+// var trips = JSON.parse(fs.readFileSync('data/trips.json', 'utf8'));
 
-const travel = (req, res) => {
-    res.render('travel', {title: 'Travlr Getaway', trips});
+const tripsEndpoint = 'http://localhost:3000/api/trips';
+const options = {
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json'
+    }
+}
+
+const travel = async function(req, res, next) {
+
+    await fetch(tripsEndpoint, options)
+    .then(res => res.json())
+    .then(json => {
+        res.render('travel', {title: 'Travlr Getaway', trips:json});
+
+    })
+    .catch(err => res.status(500).send(e.message))
+    
 };
 
 module.exports = {
