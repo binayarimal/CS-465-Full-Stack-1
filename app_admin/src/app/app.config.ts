f136e6ac32a65daaf09118ea718815cfbd140525
Binaya Rimal <rimal.binaya@gmail.com>
2024-06-23 11:35:59 -0700

    mod 7 completed

--- app_admin/src/app/app.config.ts
+++ app_admin/src/app/app.config.ts
@@ -1,9 +1,7 @@
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { HTTP_INTERCEPTORS, HttpClient, provideHttpClient } from '@angular/common/http';
-import { JwtInterceptor } from './utils/jwt.interceptor';
 import { routes } from './app.routes';
-import { authInterceptProvider } from './utils/jwt.interceptor';
 
 
 export const appConfig: ApplicationConfig = {
